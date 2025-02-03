f752fc7adb6dd5f062c02818ab12bbeffcd9c42d
Hedy-chu <hechu1213@gmail.com>
2025-02-03 14:27:35 +0800

    update

--- contracts/misc/interfaces/IWETH.sol
+++ contracts/misc/interfaces/IWETH.sol
@@ -1,7 +1,7 @@
 // SPDX-License-Identifier: AGPL-3.0
 pragma solidity ^0.8.0;
 
-interface IWIP {
+interface IWETH {
   function deposit() external payable;
 
   function withdraw(uint256) external;
